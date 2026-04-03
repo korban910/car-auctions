efc1096c7715bcfa15a4534ed7b4412c454920d3
korban <korban.torsun@gmail.com>
2026-04-03 12:40:31 -0400

    - add create/delete/update auction

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -35,4 +35,62 @@ public class AuctionsController(
         
         return mapper.Map<AuctionDto>(auction);
     }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
+    {
+        var auction = mapper.Map<Auction>(createAuctionDto);
+        
+        // TODO: add current user as seller
+        auction.Seller = "Test";
+        
+        await context.AddAsync(auction);
+
+        var result = await context.SaveChangesAsync() > 0;
+        
+        return result ?
+            CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, mapper.Map<AuctionDto>(auction)) :
+            BadRequest("Could not create auction");
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        if (await context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(a => a.Id == id)
+            is not { } auction)
+        {
+            return NotFound("Auction not found");
+        }
+        
+        // TODO: check seller == username
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+        var result = await context.SaveChangesAsync() > 0;
+        
+        return result ?
+            Ok() :
+            BadRequest("Could not update auction");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        if (await context.Auctions.FindAsync(id) is not { } auction)
+        {
+            return NotFound();
+        }
+        
+        // TODO: check seller == username
+        context.Auctions.Remove(auction);
+
+        var result = await context.SaveChangesAsync() > 0;
+        
+        return result ?
+            Ok() :
+            BadRequest("Could not delete auction");
+    }
 }
--- src/AuctionService/DTOs/UpdateAuctionDto.cs
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -2,9 +2,9 @@ namespace AuctionService.DTOs;
 
 public class UpdateAuctionDto
 {
-    public required string Make { get; set; }
-    public required string Model { get; set; }
-    public required int Year { get; set; }
-    public required string Color { get; set; }
-    public required int Mileage { get; set; }
+    public string? Make { get; set; }
+    public string? Model { get; set; }
+    public int? Year { get; set; }
+    public string? Color { get; set; }
+    public int? Mileage { get; set; }
 }
