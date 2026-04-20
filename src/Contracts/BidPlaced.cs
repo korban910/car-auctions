68da74d08c0f486e87a7f6954e7a4b676019fc79
korban <korban.torsun@gmail.com>
2026-04-20 06:20:20 -0400

    - added BidPlaced/AuctionFinished - added Consumer in AuctionService/SearchService - added new client in IdentityService

---
+++ src/Contracts/BidPlaced.cs
@@ -0,0 +1,11 @@
+namespace Contracts;
+
+public class BidPlaced
+{
+    public string Id { get; set; }
+    public string AuctionId { get; set; }
+    public string Bidder { get; set; }
+    public DateTimeOffset BidTime { get; set; }
+    public int Amount { get; set; }
+    public string BidStatus { get; set; }
+}
