a30cb551112ce5cd60f13c8507ecbe917a9097db
korban <korban.torsun@gmail.com>
2026-05-06 09:07:24 -0400

    - added fetch api - added AuctionCard - added react-countdown package

---
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import AuctionCard from "@/app/auctions/AuctionCard";
+
+async function getData() {
+  const res = await fetch('http://localhost:6001/search?pageSize=10');
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data.');
+  }
+
+  return res.json();
+}
+
+const Listings = async () => {
+  const data = await getData();
+
+  return (
+    <div className="grid grid-cols-4 gap-6">
+      {
+        data && data.results.map((auction: any) => (
+          <AuctionCard auction={auction} key={auction.id} />
+        ))
+      }
+    </div>
+  );
+};
+
+export default Listings;
